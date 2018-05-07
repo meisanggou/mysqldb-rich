1c8475a31e7c530d99eddf02d8599fbf28bfd6c4
meisanggou <zhou5315938@163.com>
2018-05-07 10:25:42 +0800

    如果任务设置error_continue不为True Pipeline有任务失败时，尝试删除已放入队列的任务

---
+++ README.md
@@ -0,0 +1,4 @@
+# mysqldb_rich
+
+## 0.1
+rich operation for mysql 
---
+++ setup.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+#  __author__ = 'meisanggou'
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import sys
+
+if sys.version_info <= (2, 7):
+    sys.stderr.write("ERROR: mysqldb-rich requires Python Version 2.7 or above.\n")
+    sys.stderr.write("Your Python Version is %s.%s.%s.\n" % sys.version_info[:3])
+    sys.exit(1)
+
+name = "mysqldb-rich"
+version = "0.1"
+url = "https://github.com/meisanggou/mysqldb-rich"
+license = "MIT"
+author = "meisanggou"
+short_description = "rich mysqldb"
+long_description = """rich operation for mysql"""
+keywords = "flask_helper"
+install_requires = [""]
+
+setup(name=name,
+      version=version,
+      author=author,
+      author_email="zhouheng@gene.ac",
+      url=url,
+      packages=["mysqldb_rich"],
+      license=license,
+      description=short_description,
+      long_description=long_description,
+      keywords=keywords,
+      install_requires=install_requires
+      )
