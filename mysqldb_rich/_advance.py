648286ffeb9f2b4c3291a624e4c4dc8823957aa4
meisanggou <zhou5315938@163.com>
2018-05-15 09:10:27 +0800

    0.6

--- mysqldb_rich/_advance.py
+++ mysqldb_rich/_advance.py
@@ -10,6 +10,7 @@ __author__ = '鹛桑够'
 
 
 class DB(ConfDB, SelectDB, InsertDB, UpdateDB, DeleteDB):
+    t_tables = "information_schema.TABLES"
 
     def __init__(self, conf_path=None, conf_dir=None, readonly=False, user=None, password=None):
         ConfDB.__init__(self, conf_path, conf_dir, readonly, user, password)
@@ -19,6 +20,14 @@ class DB(ConfDB, SelectDB, InsertDB, UpdateDB, DeleteDB):
         sql_query += ",".join(map(self.literal, args))
         sql_query += ");"
         return self.execute(sql_query)
+
+    def table_exist(self, t_name):
+        where_value = dict(TABLE_SCHEMA=self._db_name, TABLE_TYPE='BASE TABLE', TABLE_NAME=t_name)
+        cols = ["TABLE_NAME", "CREATE_TIME", "TABLE_COMMENT"]
+        l = self.execute_select(self.t_tables, where_value=where_value, cols=cols, package=False)
+        if l == 0:
+            return False
+        return True
 
     def create_user(self, user, password, host='localhost', db=None, readonly=False):
         l = self.execute_select("mysql.user", where_value=dict(user=user, host=host))
